56d72d0e54f2a091546333f1d5628b84ccc98667
Archelis <150857378+ShadowlessAsura@users.noreply.github.com>
2026-09-14 12:49:05 -0400

    Added two's complement system

--- Programming Assignment 1/program.py
+++ Programming Assignment 1/program.py
@@ -1,10 +1,14 @@
 user_value = input("Please enter a value in base 10: ")
 
-def make_binary(value):
+def make_binary(value: str) -> str:
     result = ""
     bit = 31
-    value = int(value)
+    #Allow only positive integers
+    value = abs(int(value))
     while (bit >= 0):
+        #whitespace to separate by nibbles for improved readability. 
+        if ((bit+1) % 4 == 0 and bit !=31): 
+            result +=" "
         currBit = 2**bit
         if (value>=currBit):
             result += "1"
@@ -12,17 +16,34 @@ def make_binary(value):
         else:
             result += "0"
         bit -= 1
+    return result
+#Currently not woring. E.G. value = -10. 
+def twos_complement(value: str) -> str:
+    bit = 36
+    result = ""
+    count = 0
 
-    print(f"{len(result)}\n")
+    while (count<=bit):
+        if value[count] != " ":
+            result += "0" if value[count] == "1" else "1"
+        else: 
+            result += " "
+        count += 1        
+
+    if value[31] == "1":
+        result+= "10"
+    else: 
+        result+= "01"
+
     return result
 
-def make_hex(value):
-    result = ""
-    bit = 7
-    value = int(value)
-    while(bit>=0):
-        currBit = 16**bit
-        if
-        
+def make_hex(value: str) -> str:
+    pass
 
-print(make_binary(user_value))
+
+
+        
+if (int(user_value) < 0):
+    print(f"Twos-compliment Binary: {twos_complement(make_binary(user_value))}")
+else: 
+    print(f"Binary: {make_binary(user_value)}")
